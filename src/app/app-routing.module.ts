b15d9ed03263a8a48df9210e33fda3cc86c19ad1
njilie <oriane.njilie@social.aston-ecole.com>
2020-11-02 09:29:48 +0100

    Fin project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LeaveGuard } from './guards/leave.guard';
 import { SignedGuard } from './guards/signed.guard';
 import { AuthComponent } from './pages/auth/auth.component';
 import { CatsComponent } from './pages/cats/cats.component';
